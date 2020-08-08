ce9af63687867db538fcc589033ebc68b58c2c38
DanielleEmely <danielleemily92014@gmail.com>
2020-08-08 18:16:00 -0300

    Aula 4 - Final

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 
+//dados
 const proffys = [
     {
         name: "Diego Fernandes", 
@@ -21,24 +22,90 @@ const proffys = [
         weekday: [1], 
         time_from: [720], 
         time_to: [1220] 
+    },
+    {
+        name: "Danielle Emely", 
+        avatar: "https://avatars1.githubusercontent.com/u/55715593?s=460&u=e990ec73a7a194d4b39e0f3399baceaf862c3e38&v=4",
+        whatsapp: "8999955542",
+        bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+        subject: "Química",
+        cost: "20", 
+        weekday: [1], 
+        time_from: [720], 
+        time_to: [1220] 
     }
 ]
 
+const subjects = [
+    "Artes",
+    "Biologia",
+    "Ciências",
+    "Educação física",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química",
+]
+
+const weekdays = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado",
+]
+//funcionalidades
+
+function getSubject(subjectNumber){
+    const position = +subjectNumber - 1
+    return subjects[position]
+}
+
+function pageLanding(req, res) {
+    return res.render("index.html")
+}
+
+function pageStudy(req, res) {
+    const filters = req.query
+    return res.render("study.html", { proffys, filters, subjects, weekdays})
+}
+
+function pageGiveClasses(req, res) {
+    const data = req.query
+
+    // se tver dados (dados), adicionar
+    const isNotEmpty = Object.keys(data).length > 0
+    if(isNotEmpty ){
+        data.subject = getSubject(data.subject)
+        // adicionar data a lista de proffys
+        proffys.push(data)
+        return res.redirect("/study")
+    }
+    // senao, não adicionar
+    return res.render("give-classes.html", { subjects, weekdays })
+}
+
+// Servidor
 const express = require('express')
 const server = express()
 
-server.use(express.static("public"))
-
-.get("/", (req, res) => {
-    return res.sendFile(__dirname + "/views/index.html")
+//configurar nunjuks (template engine)
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+    express: server,
+    onCahe: true,
 })
-
-.get("/study", (req, res) => {
-    return res.sendFile(__dirname + "/views/study.html")
-})
-
-.get("/give-classes", (req, res) => {
-    return res.sendFile(__dirname + "/views/give-classes.html")
-})
-
+// inicio e configuração do nosso servidor
+server
+// configurar arquivos estáticos (css, scripts, imagens)
+.use(express.static("public"))
+//rotas da aplicação
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
+// start do servidor
 .listen(5500)
